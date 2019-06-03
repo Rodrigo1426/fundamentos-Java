88e855b8bf55f7165d42ff91307897a4a5d3d9ee
rodrigo.mramos4 <rodrigo.mramos4@TAT0541240W10-1.senacsp.edu.br>
2019-06-03 19:48:27 -0300

    Ultimas atts

--- Fundamentos/src/PDV.java
+++ Fundamentos/src/PDV.java
@@ -1,14 +1,39 @@
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.util.Date;
 
 public class PDV {
 
+	double valor;
+	double desconto;
+	double troco;
+	
 	public static void main(String[] args) {
 		// Date -> trabalhar com data
 		Date data = new Date();
 		//formatar a data
 		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
 		System.out.println(formatador.format(data));
+		System.out.println("================");
+		System.out.println("======PDV=======");
+		System.out.println("================");
+		System.out.println("");
+		System.out.println("Valor total: ");
+		System.out.println("Desconto em %: ");
+		double calcular = leia.nextLine();
+		System.out.println("Total com desconto: ");
+		System.out.println("____________________________");
+		System.out.println("");
+		System.out.println("Valor pago: ");
+		System.out.println("Troco: ");
 	}
-
+	
+	private static void calcular()
+	{
+		desconto = valor / 100;
+		
+		
+	}
+	
+	
 }
